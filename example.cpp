003075f000c446ed44125acf7c6404d2fd2aecf5
trodevel <trodevel@gmail.com>
2020-09-16 03:03:44 +0200

    intermediate results

--- example.cpp
+++ example.cpp
@@ -3,6 +3,8 @@
 
 #include "handler.h"        // user_reg_handler::Handler
 #include "user_reg_email/user_reg_email.h"
+#include "user_reg_protocol/object_initializer.h"
+#include "basic_objects/object_initializer.h"
 
 #include "utils/dummy_logger.h" // dummy_logger::set_log_level
 #include "utils/log_test.h"     // log_test
@@ -55,6 +57,7 @@ void init(
         user_manager::UserManager       * um,
         user_reg::UserReg               * ur,
         user_reg_email::UserRegEmail    * ure,
+        user_reg_handler::Handler       * h,
         std::string                     * to_email,
         uint32_t expiration,
         const std::string               & config_file,
@@ -83,15 +86,38 @@ void init(
     config2.body_template   = body_template;
 
     ure->init( config2, ur );
+
+    h->init( ure );
 }
 
 bool register_user_1(
-        user_reg_email::UserRegEmail    * ure,
+        user_reg_handler::Handler       * h,
         const std::string               & email,
         user_reg::user_id_t             * user_id,
         std::string                     * error_msg )
 {
-    return ure->register_new_user( 1, email, "\xff\xff\xff", user_id, error_msg );
+    std::unique_ptr<user_reg_protocol::RegisterUserRequest> req( new user_reg_protocol::RegisterUserRequest );
+
+    user_reg_protocol::User u;
+
+    basic_objects::Email e;
+    basic_objects::Date d;
+
+    basic_objects::initialize( & e, email );
+    basic_objects::initialize( & d, 2020, 9, 16 );
+
+    user_reg_protocol::initialize( & u,
+            basic_objects::gender_e::UNDEF,
+            "johndoe",
+            "Doe",
+            "John",
+            e,
+            "+123",
+            d );
+
+    user_reg_protocol::initialize( req.get(), u, "secret" );
+
+    auto resp = h->handle( 0, * req.get() );
 }
 
 void test_kernel( const std::string & name, const std::string & config_file, const std::string & subject, const std::string & body_template )
@@ -99,15 +125,16 @@ void test_kernel( const std::string & name, const std::string & config_file, con
     user_manager::UserManager   um;
     user_reg::UserReg ur;
     user_reg_email::UserRegEmail ure;
+    user_reg_handler::Handler h;
 
     std::string to_email;
 
-    init( & um, & ur, & ure, & to_email, 1, config_file, subject, body_template );
+    init( & um, & ur, & ure, & h , & to_email, 1, config_file, subject, body_template );
 
     user_reg::user_id_t user_id;
     std::string         error_msg;
 
-    auto b = register_user_1( & ure, to_email, & user_id, & error_msg );
+    auto b = register_user_1( & h, to_email, & user_id, & error_msg );
 
     log_test( name, b, true, "user was added", "cannot add user", error_msg );
 }
